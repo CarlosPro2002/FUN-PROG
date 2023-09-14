d57dc8a23e69dda4326796bee439220dd6a6f6a6
CarlosPro2002 <143568967+CarlosPro2002@users.noreply.github.com>
2023-09-13 19:45:11 -0600

    Add files via upload

---
+++ EVA1_10_IDENTIFICADORES/src/main/java/com/mycompany/eva1_10_identificadores/EVA1_10_IDENTIFICADORES.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_10_identificadores;
+
+/**
+ *
+ * @author carlo
+ */
+public class EVA1_10_IDENTIFICADORES {
+
+    public static void main(String[] args) {
+        //TODO code application logic here
+        //int 1salario; no cumple con las reglas: inicia con numero
+        //int salario&; no cumple: & no es caracter alfanumerico 
+        int mi_salario;
+        int $salario;
+        int salario1000;
+        //int _; no es valido
+        int $;
+        //int mi_salario; no valido, espacio en blanco 
+        
+        int salario;
+        int Salario;
+        int SALARIO;
+        //int salario;  no podemos repetir identificadores 
+        
+        //int public; no podemos usar keywords (palabras reservadas)
+        
+        int println; 
+        
+        
+    }
+}
