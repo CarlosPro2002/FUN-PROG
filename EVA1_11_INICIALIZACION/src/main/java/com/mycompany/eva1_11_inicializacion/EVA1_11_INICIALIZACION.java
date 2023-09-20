fef5b475c524d9a05534d4ce5bfa711d021cd8fc
CarlosPro2002 <143568967+CarlosPro2002@users.noreply.github.com>
2023-09-19 19:50:03 -0600

    Add files via upload

---
+++ EVA1_11_INICIALIZACION/src/main/java/com/mycompany/eva1_11_inicializacion/EVA1_11_INICIALIZACION.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_11_inicializacion;
+
+/**
+ *
+ * @author carlo
+ */
+public class EVA1_11_INICIALIZACION {
+
+    public static void main(String[] args) {
+        //TODO code application logic here
+        //Los parentersis denotan una funcion: 
+        //println() 
+        System.out.println("Hola Mundo");
+        int valor = 0; //Inicializar la varieble para utilizarla
+        System.out.println(valor);
+        
+        //declaramos e inicializamos 3 variables double 
+        double salario = 0, iva = 0.16, subtotal = 10000; 
+        
+        final int CALIFA_PASAR = 70;
+        final String ESCUELA = "Instituto Tecnologico de Chihuahua ll";
+        
+        System.out.println(ESCUELA);
+        System.out.println("La califa minima para pasar es:");
+        System.out.println(CALIFA_PASAR);
+        
+        
+        //errores por que solo se puede una sola vez 
+        //califa_pasar = 100;
+        //califa_pasar = 20; 
+    }
+}
