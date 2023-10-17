cf4d73ca296a14da46b44d8fda0000b5774d291a
CarlosPro2002 <143568967+CarlosPro2002@users.noreply.github.com>
2023-10-16 19:29:29 -0600

    Add files via upload

---
+++ EVA2_6_AND_OR_OPERADORES/src/main/java/com/mycompany/eva2_6_and_or_operadores/EVA2_6_AND_OR_OPERADORES.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_6_and_or_operadores;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author carlo
+ */
+public class EVA2_6_AND_OR_OPERADORES {
+
+    public static void main(String[] args) {
+        int califa; 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduce la calificacion: ");
+        califa = input.nextInt ();
+        
+        //VALIDAR
+        //UNA CALIFA ES MAYOR O IGUAL A 0 --> califa >=0
+        //Y --> &&
+        //LA CALIFA ES MENOR O IGUAL A 100 --> califa <=0
+        //0 A 100
+        
+        if ((califa >= 0) && (califa <= 100 )){//valor valido 
+            System.out.println("La calificacion" + califa + "es valida!!");
+            if (califa >= 70)
+                System.out.println("APROBADO");
+            else 
+                System.out.println("NO ACREDITADO");
+        }else{
+            System.out.println("La calificacion " + califa + "no es valida!!");
+        }
+        
+        
+    }
+}
