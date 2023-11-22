af718d7ddbc94a358a85f9d4cb7de78f88302dbc
CarlosPro2002 <143568967+CarlosPro2002@users.noreply.github.com>
2023-11-21 19:51:41 -0600

    Add files via upload

---
+++ EVA3_1_FUNCIONES/src/main/java/com/mycompany/eva3_1_funciones/EVA3_1_FUNCIONES.java
@@ -0,0 +1,26 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_1_funciones;
+
+/**
+ *
+ * @author carlo
+ */
+public class EVA3_1_FUNCIONES {
+
+    public static void main(String[] args) {
+       imprimirMensaje("HOLA MUNDO");
+   
+    
+    
+    }
+    public static  void imprimirMensaje(String mensaje){
+        System.out.println(mensaje);
+    }
+    
+    
+
+}
